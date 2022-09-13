a35b6879ad8ac4287b37b835c450ad8dda28476d
Alireza <a.z.nikravesh@outlook.com>
2022-09-13 22:39:20 +0430

    - added a api for integeration test with client. - added swagger

---
+++ Backend/Accounting/Accounting.WebApi/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting.WebApi.Controllers;
+
+/// <summary>
+/// A class for registration users
+/// </summary>
+
+[Route("api/[controller]")]
+[ApiController]
+public class RegisterController : ControllerBase
+{
+    private readonly ILogger<RegisterController> logger;
+
+    public RegisterController(ILogger<RegisterController> logger)
+    {
+        this.logger = logger;
+    }
+
+    /// <summary>
+    /// Register new users 
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<IActionResult> RegisterUser()
+    {
+        return Ok("Registration is Succeed!");
+    }
+}
