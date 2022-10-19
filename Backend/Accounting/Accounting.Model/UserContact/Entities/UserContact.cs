004c82b560283fbdd272bd4fea4b0b214fc184e3
Mohammad Zarei <115799428+Mohammadzarei754@users.noreply.github.com>
2022-10-19 18:19:54 +0330

    Add user contact entity

---
+++ Backend/Accounting/Accounting.Model/UserContact/Entities/UserContact.cs
@@ -0,0 +1,12 @@
+﻿using Accounting.Model.Users.Entities;
+
+public class UserContact
+{
+    public int Id { get; set; }
+    public int OwnerId { get; set; }
+    public int ContactId { get; set; }
+
+    public virtual ICollection<User> Owner { get; set; }
+    public virtual ICollection<User> Contact { get; set; }
+
+}
